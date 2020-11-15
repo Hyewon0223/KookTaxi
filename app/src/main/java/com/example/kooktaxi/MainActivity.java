3fa1f9ccb7828986485c817f0cd813443656b56a
leehanjeong <rosy0111@naver.com>
2020-11-16 02:02:14 +0900

    Main 주석2

--- app/src/main/java/com/example/kooktaxi/MainActivity.java
+++ app/src/main/java/com/example/kooktaxi/MainActivity.java
@@ -167,6 +167,8 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     };
 
+
+
     private void startLocationUpdates() {
         if (!checkLocationServicesStatus()) {  //위치설정이 되지 않았으면
             showDialogForLocationServiceSetting();  //위치설정을 한다
@@ -224,7 +226,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
-    public boolean checkLocationServicesStatus() {
+    public boolean checkLocationServicesStatus() { //locationManager를 통해 GPS와 네트워크가 사용 가능한지  체크해주는 함수
         LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
 
         return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)
@@ -232,22 +234,22 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     public void setCurrentLocation(Location location, String markerTitle, String markerSnippet) {
-        if (currentMarker != null) currentMarker.remove();
-        LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude());
+        if (currentMarker != null) currentMarker.remove(); //마커가 존재하면 마커를 지운다
+        LatLng currentLatLng = new LatLng(location.getLatitude(), location.getLongitude()); //현재 위치
 
-        MarkerOptions markerOptions = new MarkerOptions();
+        MarkerOptions markerOptions = new MarkerOptions(); //마커 옵션 설정
         markerOptions.position(currentLatLng);
         markerOptions.title(markerTitle);
         markerOptions.snippet(markerSnippet);
         markerOptions.draggable(true);
 
-        currentMarker = mMap.addMarker(markerOptions);
+        currentMarker = mMap.addMarker(markerOptions); //해당 마커 추가
 
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(currentLatLng);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLng(currentLatLng); //카메라를 현재 위치로 이동
         mMap.moveCamera(cameraUpdate);
     }
 
-    public void setDefaultLocation() {  //Default MarkerOptions를 설정해주는 함수
+    public void setDefaultLocation() {  //Default MarkerOptions를 설정해주는 함수(위치정보 제공이 허용되어 있지 않은 경우)
         LatLng DEFAULT_LOCATION = new LatLng(37.52487, 126.92723);  //첫 위치를 서울(여의도 부근)으로 설정
         String markerTitle = "위치정보 가져올 수 없음";
         String markerSnippet = "위치 퍼미션과 GPS 활성 요부 확인하세요";
@@ -266,6 +268,7 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         mMap.moveCamera(cameraUpdate);
     }
 
+    //런타임 퍼미션 처리 메소드들
     private boolean checkPermission() {
         int hasFineLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
         int hasCoarseLocationPermission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
@@ -275,33 +278,32 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         return false;
     }
 
-
     @Override
-    public void onRequestPermissionsResult(int permsRequestCode, @NonNull String[] permissions, @NonNull int[] grandResults) {
-        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) {
+    public void onRequestPermissionsResult(int permsRequestCode, @NonNull String[] permissions, @NonNull int[] grandResults) { //ActivityCompat.requestPermissions를 사용한 퍼미션 요청의 결과를 리턴받는 메소드.
+        if (permsRequestCode == PERMISSIONS_REQUEST_CODE && grandResults.length == REQUIRED_PERMISSIONS.length) { //요청 코드가 PERMISSIONS_REQUEST_CODE이고, 요청한 퍼미션 개수만큼 수신되었다면
             boolean check_result = true;
 
-            for (int result : grandResults) {
+            for (int result : grandResults) { //모든 퍼미션을 허용했는지 체크한다.
                 if (result != PackageManager.PERMISSION_GRANTED) {
                     check_result = false;
                     break;
                 }
             }
-            if (check_result) {
-                startLocationUpdates();
+            if (check_result) { //모든 퍼미션을 허용했으면
+                startLocationUpdates(); //위치 업데이트를 시작한다
             }
-            else {
-                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0])
-                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) {
+            else { //거부한 퍼미션이 있다면
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[0]) //REQUIRED_PERMISSIONS = {Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION}
+                        || ActivityCompat.shouldShowRequestPermissionRationale(this, REQUIRED_PERMISSIONS[1])) { //사용자가 거부만 선택한 경우
                     Snackbar.make(mLayout, "퍼미션이 거부되었습니다. 앱을 다시 실행하여 퍼미션을 허용해주세요.",
-                            Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
+                            Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() { //앱을 다시 실행해서 허용을 선택하면 된다는 Snackbar를 띄운다
                         @Override
                         public void onClick(View view) {
                             finish();
                         }
                     }).show();
-                }else {
-                    Snackbar.make(mLayout, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ",
+                }else { //"다시 묻지 않음"을 선택한 경우
+                    Snackbar.make(mLayout, "퍼미션이 거부되었습니다. 설정(앱 정보)에서 퍼미션을 허용해야 합니다. ", //설정에서 직접 퍼미션을 허용해야 한다는 Snackbar를 띄운다.
                             Snackbar.LENGTH_INDEFINITE).setAction("확인", new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
@@ -313,19 +315,20 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         }
     }
 
+    //GPS 활성화를 위한 메소드들
     private void showDialogForLocationServiceSetting() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setTitle("위치 서비스 비활성화");
-        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다.");
-        builder.setCancelable(true);
-        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this); //AlertDialog는일부 화면만 가리는 윈도우. 정보 입력받을 수 있음
+        builder.setTitle("위치 서비스 비활성화"); //Dialog 제목
+        builder.setMessage("앱을 사용하기 위해서는 위치 서비스가 필요합니다."); //메세지
+        builder.setCancelable(true); //배경이나 뒤로가기 키 누르면 취소 가능
+        builder.setPositiveButton("설정", new DialogInterface.OnClickListener() { //positive button을 설정버튼으로 설정
             @Override
             public void onClick(DialogInterface dialog, int id) {
                 Intent callGPSSettingIntent = new Intent(android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
                 startActivityForResult(callGPSSettingIntent, GPS_ENABLE_REQUEST_CODE);
             }
         });
-        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+        builder.setNegativeButton("취소", new DialogInterface.OnClickListener() { //negative button을 취소 버튼으로 설정
             @Override
             public void onClick(DialogInterface dialog, int id) {
                 dialog.cancel();
@@ -334,12 +337,14 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
         builder.create().show();
     }
 
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+
         switch (requestCode) {
             case GPS_ENABLE_REQUEST_CODE:
-                if (checkLocationServicesStatus()) {
+                if (checkLocationServicesStatus()) { //사용자가 GPS 활성 시켰는지 검사
                     needRequest = true;
                     return;
                 }
