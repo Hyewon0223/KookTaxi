f28f778ec3951df1ecc5c443094c6bf80ec84e5d
Hyewon23 <ch2w2006@kookmin.ac.kr>
2020-11-18 03:01:12 +0900

    test

--- app/src/main/java/com/example/kooktaxi/LoginActivity.java
+++ app/src/main/java/com/example/kooktaxi/LoginActivity.java
@@ -4,17 +4,17 @@
  */
 package com.example.kooktaxi;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.view.Menu;
-import android.view.MenuInflater;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
